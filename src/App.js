5c6f55762e6227927563c25633c09960ddbeb629
Rachel Thompson <thompson.1657@gmail.com>
2021-06-24 17:33:34 -0500

    added timer functionality

--- src/App.js
+++ src/App.js
@@ -1,39 +1,81 @@
-import logo from "./logo.svg";
+import React, { useState, useRef, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [timerDays, setTimerDays] = useState("00");
+  const [timerHours, setTimerHours] = useState("00");
+  const [timerMinutes, setTimerMinutes] = useState("00");
+  const [timerSeconds, setTimerSeconds] = useState("00");
+
+  let interval = useRef();
+
+  const startTimer = () => {
+    const countdownDate = new Date("August 8 , 2021 00:00:00").getTime();
+
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countdownDate - now;
+
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      if (distance < 0) {
+        //stop timer
+        clearInterval(interval.current);
+      } else {
+        //update timer
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      }
+    }, 1000);
+  };
+
+  //componentDidMount
+  useEffect(() => {
+    startTimer();
+    return () => {
+      clearInterval(interval.current);
+    };
+  });
+
   return (
     <section className="timer-container">
       <section className="timer">
         <div>
           <span className="mdi mdi-calender-clock timer-icon"></span>
           <h2>Countdown Timer</h2>
-          <p>Countdown to your special date!</p>
+          <p>Countdown to my Birthday!</p>
         </div>
         <div>
           <section>
-            <p>87</p>
+            <p>{timerDays}</p>
             <p>
               <small>Days</small>
             </p>
           </section>
           <span>:</span>
           <section>
-            <p>87</p>
+            <p>{timerHours}</p>
             <p>
               <small>Hours</small>
             </p>
           </section>
           <span>:</span>
           <section>
-            <p>87</p>
+            <p>{timerMinutes}</p>
             <p>
               <small>Minutes</small>
             </p>
           </section>
           <span>:</span>
           <section>
-            <p>87</p>
+            <p>{timerSeconds}</p>
             <p>
               <small>Seconds</small>
             </p>
